d85bcb1992d30337335381b09f3fb9a0985f65eb
TheoLeao <theoleaoboudier@gmail.com>
2021-04-09 09:32:26 +0200

    making navigation menu

---
+++ components/GlobalHead/GlobalHead.tsx
@@ -0,0 +1,13 @@
+
+import Head from 'next/head'
+export const GlobalHead = () => {
+    return (
+        <>
+            <Head>
+                
+            </Head>
+        </>
+    )
+}
+
+export default GlobalHead;
---
+++ components/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import styles from './HomePage.module.scss'
+import NavigationMenu from './../NavigationMenu/NavigationMenu'
+const HomePage = () => {
+    return (
+        <>
+            <NavigationMenu></NavigationMenu>
+        </>
+    )
+}
+export default HomePage
