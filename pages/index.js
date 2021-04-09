f5a5b2b86f276e65db6055cbf400b3aa3558ceb5
theoleao <theoleaoboudier@gmail.com>
2021-04-09 12:20:58 +0200

    fetching all data: projects, categories, pages via GraphQL from Wordpress CMS Headless

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,12 @@ import HomePage from '../components/HomePage/HomePage'
 import getAllCategories from '../lib/api'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export default function Home({ allCategories }) {
+export default function Home({ data }) {
+  console.log(data)
   return (
     <>
       <GlobalHead></GlobalHead>
-      <HomePage allCategories={allCategories}></HomePage>
+      <HomePage allCategories={data.categories.allCategories}></HomePage>
     </>
 
   )
@@ -27,7 +28,7 @@ export async function getStaticProps() {
     cache: new InMemoryCache()
   });
 
-  //Queries
+  //Categories Queries
   const allCategories = await client.query({
     query: gql`
   query GetAllCategories {
@@ -46,14 +47,390 @@ export async function getStaticProps() {
   }
   `
   });
+  const allProjectsCategories = await client.query({
+    query: gql`
+    query GetProjectsCategories {
+      categories(where: {childOf: 10}) {
+        nodes {
+          name
+          categoryId
+          parent {
+            node {
+              categoryId
+              name
+            }
+          }
+        }
+      }
+    }
+    
+  `
+  });
+  const allBlogsCategories = await client.query({
+    query: gql`
+    query GetBlogsCategories {
+      categories(where: {childOf: 10}) {
+        nodes {
+          name
+          categoryId
+          parent {
+            node {
+              categoryId
+              name
+            }
+          }
+        }
+      }
+    } 
+  `
+  });
 
-  
+  //Projects Queries
+  const allAudiovisuelsProjects = await client.query({
+    query: gql`
+    query GetAudiovisuelsProjects {
+      projects(where: {categoryId: 13}) {
+        edges {
+          node {
+            title
+            projectInfos {
+              projectLink
+              projectShortDescription
+              projectLongDescription
+              projectToolsused
+              projectMainImage {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectTotalPreviewImages
+              projectPreviewImage1 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage2 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage3 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage4 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage5 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectThumbail {
+                altText
+                mediaItemUrl
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+    
+  `
+  });
+  const allWebProjects = await client.query({
+    query: gql`
+    query GetWebProjects {
+      projects(where: {categoryId: 13}) {
+        edges {
+          node {
+            title
+            projectInfos {
+              projectLink
+              projectShortDescription
+              projectLongDescription
+              projectToolsused
+              projectMainImage {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectTotalPreviewImages
+              projectPreviewImage1 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage2 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage3 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage4 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage5 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectThumbail {
+                altText
+                mediaItemUrl
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+    
+  `
+  });
+  const allGraphicsProjects = await client.query({
+    query: gql`
+    query GetGraphicsProjects {
+      projects(where: {categoryId: 13}) {
+        edges {
+          node {
+            title
+            projectInfos {
+              projectLink
+              projectShortDescription
+              projectLongDescription
+              projectToolsused
+              projectMainImage {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectTotalPreviewImages
+              projectPreviewImage1 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage2 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage3 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage4 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage5 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectThumbail {
+                altText
+                mediaItemUrl
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    
+  `
+  });
+  const allMarketingProjects = await client.query({
+    query: gql`
+    query GetMarketingProjects {
+      projects(where: {categoryId: 13}) {
+        edges {
+          node {
+            title
+            projectInfos {
+              projectLink
+              projectShortDescription
+              projectLongDescription
+              projectToolsused
+              projectMainImage {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectTotalPreviewImages
+              projectPreviewImage1 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage2 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage3 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage4 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectPreviewImage5 {
+                altText
+                mediaItemUrl
+                title
+              }
+              projectThumbail {
+                altText
+                mediaItemUrl
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+    
+  `
+  });
 
+  //Pages Queries
+  const allHomePageData = await client.query({
+    query: gql`
+    query GetHomepageData {
+      homepageInfos {
+        edges {
+          node {
+            homepageInfos {
+              headerBitmoji {
+                altText
+                mediaItemUrl
+                title
+              }
+              headerHelloTitle
+              headerJobTitle
+              headerJobTitleEmoji {
+                altText
+                mediaItemUrl
+                title
+              }
+              headerDescription
+              headerButtonActionUrl
+              headerButtonActionText
+              careerSectionTitle
+              careerTotalItems
+              careerItem1Date
+              careerItem1Description
+              careerItem2Date
+              careerItem2Description
+              careerItem3Date
+              careerItem3Description
+              careerItem4Date
+              careerItem4Description
+              careerItem5Date
+              careerItem5Description
+              careerItem6Date
+              careerItem6Description
+              categoriesSectionTitle
+              categoriesItemButtonText
+              featuredProjectSectionTitle
+              featuredProjectTitle
+              featuredProjectShortDescription
+              featuredProjectPostUrl
+              featuredProjectPostDate
+              featuredProjectPostAuthor
+              featuredProjectButtonText
+              trustedSectionTitle
+              trustedCompanyItem1Title
+              trustedCompanyItem1Logo {
+                altText
+                mediaItemUrl
+                title
+              }
+              trustedCompanyItem1Shortdescription
+              trustedCompanyItem1Url
+              trustedCompanyItem2Title
+              trustedCompanyItem2Logo {
+                altText
+                mediaItemUrl
+                title
+              }
+              trustedCompanyItem2Shortdescription
+              trustedCompanyItem2Url
+              trustedCompanyItem3Title
+              trustedCompanyItem3Logo {
+                altText
+                mediaItemUrl
+                title
+              }
+              trustedCompanyItem3Shortdescription
+              trustedCompanyItem3Url
+              trustedCompanyItem4Title
+              trustedCompanyItem4Logo {
+                altText
+                mediaItemUrl
+                title
+              }
+              trustedCompanyItem4Shortdescription
+              trustedCompanyItem4Url
+              trustedCompanyItem5Title
+              trustedCompanyItem5Logo {
+                altText
+                mediaItemUrl
+                title
+              }
+              trustedCompanyItem5Shortdescription
+              trustedCompanyItem5Url
+              trustedSectionTitleEmoji {
+                altText
+                mediaItemUrl
+                title
+              }
+            }
+          }
+        }
+      }
+    }    
+  `
+  });
 
 
   return {
     props: {
-      allCategories: allCategories.data,
+      data: {
+        categories: {
+          allCategories: allCategories.data,
+          allProjectsCategories: allProjectsCategories.data,
+          allBlogsCategories: allBlogsCategories.data,
+        },
+        projects: {
+          web: allWebProjects,
+          audiovisuel: allAudiovisuelsProjects.data,
+          graphics: allGraphicsProjects.data,
+          marketing: allMarketingProjects.data
+        },
+        pages: {
+          homepage: allHomePageData.data
+        }
+      },
 
     }
   }
