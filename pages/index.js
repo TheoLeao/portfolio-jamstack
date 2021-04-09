9b6dccd6cdd4e16075d51d22d9f3b8be26c66968
theoleao <theoleaoboudier@gmail.com>
2021-04-09 15:50:36 +0200

    update fetching

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,51 @@ import GlobalHead from '../components/GlobalHead/GlobalHead'
 import HomePage from '../components/HomePage/HomePage'
 
 //Data Fetching
-import { GetHomepageData } from '../lib/api'
+import { GetAllCategories, GetAllProjectsCategories, GetAllBlogsCategories, GetAllAudiovisuelsProjects, GetAllWebProjects, GetAllGraphicsProjects, GetAllMarketingProjects, GetHomepageData } from '../lib/api'
 
-export default function Home({ homePageData }) {
-  console.log(homePageData)
+export default function Home({ data }) {
+  console.log(data)
   return (
     <>
       <GlobalHead></GlobalHead>
-      <HomePage homePageData={homePageData}></HomePage>
+      <HomePage homePageData={data.homePageData}></HomePage>
     </>
 
   )
 }
 
 export async function getStaticProps() {
-  const homePageData = await GetHomepageData()
+  const allCategories = await GetAllCategories();
+  const allProjectsCategories = await GetAllProjectsCategories();
+  const allBlogsCategories = await GetAllBlogsCategories();
+
+  const allWebProjects = await GetAllWebProjects();
+  const allAudiovisuelsProjects = await GetAllAudiovisuelsProjects();
+  const allGraphicsProjects = await GetAllGraphicsProjects();
+  const allMarketingProjects = await GetAllMarketingProjects();
+
+  const homePageData = await GetHomepageData();
   return {
-    props: { 
-      homePageData: homePageData.data },
+    props: {
+      data: {
+        categories: {
+          allCategories: allCategories.data,
+          allProjectsCategories: allProjectsCategories.data,
+          allBlogsCategories: allBlogsCategories.data,
+        },
+        projects: {
+          allWebProjects: allWebProjects.data,
+          allAudiovisuelsProjects: allAudiovisuelsProjects.data,
+          allGraphicsProjects: allGraphicsProjects.data,
+          allMarketingProjects: allMarketingProjects.data
+          
+
+        },
+        pages: {
+          homepage: homePageData.data,
+        }
+      }
+
+    }
   }
 }
